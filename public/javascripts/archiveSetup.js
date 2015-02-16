b46caf111d23281d73d777c3f087fcb65660ec15
ankrt <ankrt@mail.com>
2015-02-16 12:00:37 +0000

    Buttons group properly on archive page

--- public/javascripts/archiveSetup.js
+++ public/javascripts/archiveSetup.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 });
 
 
+
 // FUNCTIONS
 
 rec.loadRecordings = function() {
@@ -17,66 +18,77 @@ rec.loadRecordings = function() {
 rec.generateHTML = function(data) {
     $.each(data, function() {
         recording = $([
-        "<div class='row'>",
+        //"<div class='row'>",
             "<div class='panel panel-default'>",
                 "<div class='panel-heading'>",
                     "<h4 class='panel-title'>" + this.description + "</h4>",
                 "</div>",
-                "<div class='panel-body'>",
-                    "<div class='col-sm-6'>",
+                "<div class='panel-body'>", // Pannel Body
+                "<div class='row'>", // Row
+
+                    // Column 1
+                    "<div class='col-xs-12 col-sm-4'>",
                         "<p>" + this.stationName + "</p>",
                         "<p>" + this.dateAdded + "</p>",
                         "<p>" + this.duration + " minutes</p>",
                         "<p>" + this.views + "</p>",
-                    "</div>", // END COL 1
-                    "<div class='col-sm-6'>",
-                        "<div class='panel panel-default'>",
-                            "<div class='panel-body'>",
-                            "<row>",
-                            "<div class='col-sm-4'>",
-                                // Play-Pause Toggle
-                                "<div class='btn-group' role='toolbar'>",
-                                "<audio preload='none' type='audio/mp3' src='/archive/recordings/download/" + this.file + ".mp3'>Browser playback not supported</audio>",
-                                "<button type='button' class='btn btn-default play-pause-toggle'>",
-                                    "<span class='glyphicon glyphicon-play'/>",
-                                "</button>",
-                                // Volume Down
-                                "<button type='button' class='btn btn-default vol-down'>",
-                                    "<span class='glyphicon glyphicon-volume-down'/>",
-                                "</button>",
-                                // Volume Up
-                                "<button type='button' class='btn btn-default vol-up'>",
-                                    "<span class='glyphicon glyphicon-volume-up'/>",
-                                "</button>",
-                                "</div>",
-                            "</div>",
-                            // Progress bar for audio track
-                            "<div class='col-sm-6'>",
-                            "<div class='progress'>",
-                            "<div class='progress-bar' role='progressbar' aria-valuemin='0' aria-value-max='100' style='width: 0%;'>",
-                                "<span class='sr-only'>Track progress bar</span>",
-                            "</div>",
-                            "</div>",
-                            "</div>",
-                            "<div class='col-sm-1'>",
-                                "<form method='get' action='/archive/recordings/download/" + this.file + ".mp3'>",
-                                    "<button type='submit' class='btn btn-default'>",
-                                        "<span class='glyphicon glyphicon-download-alt'/>",
-                                    "</button>",
-                                "</form>",
-                            "</div>",
-                            "</row>",
-                            "</div>",
-                        "</div>",
-                        "<p>" + this.tags + "</p>",
-                        //"<a href='/archive/recordings/download/" + this.file + ".mp3'>Download</a>",
-                    "</div>", // END COL 2
-                "</div>",
-            "</div>",
-        "</div>"
+                    "</div>",
+
+                    // Column 2
+                    "<div class='col-xs-12 col-sm-8'>",
+                        "<div class='well'>",
+
+                                // Audio control
+                                "<div class='btn-toolbar' role='toolbar'>",
+                                    "<div class='btn-group' role='group'>",
+                                        "<audio preload='none' type='audio/mp3' src='/archive/recordings/download/" + this.file + ".mp3'>Browser playback not supported</audio>",
+                                        // Play-Pause Toggle
+                                        "<button type='button' class='btn btn-default play-pause-toggle'>",
+                                            "<span class='glyphicon glyphicon-play'/>",
+                                        "</button>",
+                                        // Volume Down
+                                        "<button type='button' class='btn btn-default vol-down'>",
+                                            "<span class='glyphicon glyphicon-volume-down'/>",
+                                        "</button>",
+                                        // Volume Up
+                                        "<button type='button' class='btn btn-default vol-up'>",
+                                            "<span class='glyphicon glyphicon-volume-up'/>",
+                                        "</button>",
+                                    "</div>",
+
+                                    // Progress bar
+                                    "<div class='btn-group' role='group'>",
+                                        "<div class='progress'>",
+                                            "<div class='progress-bar' role='progressbar' aria-valuemin='0' aria-value-max='100' style='width: 0%;'>",
+                                                "<span class='sr-only'>Track progress bar</span>",
+                                            "</div>",
+                                        "</div>",
+                                    "</div>",
+
+                                    // Download Button
+                                    "<div class='btn-group pull-right' role='group'>",
+                                        "<form method='get' action='/archive/recordings/download/" + this.file + ".mp3'>",
+                                            "<button type='submit' class='btn btn-default'>",
+                                                "<span class='glyphicon glyphicon-download-alt'/>",
+                                            "</button>",
+                                        "</form>",
+                                    "</div>",
+
+                                "</div>", // End btn-toolbar
+
+
+                        "</div>", // End well
+
+                    "</div>", // END Row
+                "</div>", // END Panel Body
+            "</div>"
+        //"</div>"
         ].join("\n"));
 
         $("#recording-list").append(recording);
+
+        var progWidth = $('#recording-list .well').width() - 20 - 118 - 42;
+        $('#recording-list .progress').css('width', progWidth);
     });
 }
 
