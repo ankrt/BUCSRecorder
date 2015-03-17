5a1ef1c7ff9b27872f30cc7ec8ae9808cea34a61
ankrt <ankrt@mail.com>
2015-03-17 13:53:14 +0000

    Trimmer improvements
    Trimmed sections of recording are highlighted
    Added a play tracker that moves between the two sliders
    Play tracker automatically resets to the start of the left slider

--- public/javascripts/trim.js
+++ public/javascripts/trim.js
@@ -8,6 +8,14 @@ var totalDuration = 0;
 var trimStartTime = 0;
 var trimEndTime = 0;
 
+// store the percentage that the play tracker should move to
+// when mouseup event occurs
+var audioStartingPercentage = 0;
+var audioSkipToPercentage = 0;
+// store the maximum percentage the audio should be allowed to reach
+// updated by moving the right slider
+var audioMaxPercentage = 100;
+
 var Lactive = false;
 var Ractive = false;
 
@@ -22,22 +30,50 @@ $('audio').on('loadedmetadata', function() {
 });
 
 /*
- * Toggle between play and pause icons in the
- * play/pause button
+ * Media Controls
  */
 $('button.play-pause-toggle').on('click', function() {
-    var me = $(this); // only used within callbacks
-    var paused = audio.paused;
-    var oldState = !paused ? 'glyphicon-pause' : 'glyphicon-play';
-    var newState = paused ? 'glyphicon-pause' : 'glyphicon-play';
-
-    $(this).children('span').removeClass(oldState);
-    $(this).children('span').addClass(newState);
-
-    if (paused) {
+    if (audio.paused) {
         audio.play();
     } else {
         audio.pause();
+    }
+});
+$('button.reset').on('click', function() {
+    if (audio.playing) {
+        audio.pause();
+    }
+    changeCurrentTime(audioStartingPercentage);
+});
+$('button.volume-down').on('click', function() {
+    // decrement the volume
+    if (audio.volume > 0.1) {
+        audio.volume -= 0.1;
+    }
+    // at min volume
+    if (audio.volume < 0.05) {
+        // disable vol up
+        $(this).addClass('disabled');
+    }
+    // nolonger at max volume
+    if (audio.volume < 0.95) {
+        // enable volume down
+        $(this).next().removeClass('disabled');
+    }
+});
+$('button.volume-up').on('click', function() {
+    if (audio.volume < 1) {
+        audio.volume += 0.10;
+    }
+    // at max volume
+    if (audio.volume > 0.9) {
+        // disable vol up
+        $(this).addClass('disabled');
+    }
+    // nolonger at min volume
+    if (audio.volume > 0.05) {
+        // enable volume down
+        $(this).prev().removeClass('disabled');
     }
 });
 
@@ -45,13 +81,30 @@ $('button.play-pause-toggle').on('click', function() {
  * Advance the play tracker on timeupdate events
  */
 $('audio').on('timeupdate', function(event) {
-
     // calculate the current time as a percentage of track length
     var currentTime_pcnt = (audio.currentTime / audio.duration) * 100;
     // move the play tracker to the position that is this percentage along the timeline
-    $('.play-tracker').css('left', Math.round(currentTime_pcnt));
+    $('.play-tracker').css('left', currentTime_pcnt + '%');
+    // pause the audio if it has gone beyond the right slider and reset
+    if (currentTime_pcnt > audioMaxPercentage) {
+        audio.pause();
+        changeCurrentTime(audioStartingPercentage);
+    }
+});
 
-    //console.log(currentTime_pcnt);
+/*
+ * Change glyphicon of play/pause button to reflect whether or not the audio
+ * actually playing
+ */
+$('audio').on('playing', function(event) {
+    var target = $('button.play-pause-toggle');
+    target.children('span').removeClass('glyphicon-play');
+    target.children('span').addClass('glyphicon-pause');
+});
+$('audio').on('pause', function(event) {
+    var target = $('button.play-pause-toggle');
+    target.children('span').removeClass('glyphicon-pause');
+    target.children('span').addClass('glyphicon-play');
 });
 
 
@@ -78,6 +131,11 @@ function RmouseDown() {
 function mouseUp(event) {
     if (Lactive) {
         moveLeftSlider(event);
+        if (audio.playing) {
+            audio.pause();
+        }
+        // some bugs to iron out here... moving slider while playing etc
+        changeCurrentTime(audioStartingPercentage);
         $(window).off('mousemove', moveLeftSlider);
     } else if (Ractive) {
         moveRightSlider(event);
@@ -101,20 +159,23 @@ function moveLeftSlider(event) {
     if (newPosLeft_pcnt >= 0 && newPosLeft_pcnt <= 100) {
         fixedPosLeft_pcnt = 100 - newPosLeft_pcnt;
         Lslider.css('right', fixedPosLeft_pcnt + '%');
-    }
-    if (newPosLeft_pcnt < 0) {
+    } else if (newPosLeft_pcnt < 0) {
         fixedPosLeft_pcnt = 100;
         Lslider.css('right', fixedPosLeft_pcnt + '%');
-    }
-    if (newPosLeft_pcnt > posOtherSlider_pcnt) {
+    } else if (newPosLeft_pcnt > posOtherSlider_pcnt) {
         fixedPosLeft_pcnt = 100 - posOtherSlider_pcnt;
         Lslider.css('right', fixedPosLeft_pcnt + '%');
     }
 
-    // finally, change the width of the Ltrim-zone so it
+    // change the width of the Ltrim-zone so it
     // fills from the left up to the Lslider
     var trimZoneWidth = 100 - fixedPosLeft_pcnt;
     LtrimZone.css('width', trimZoneWidth + '%');
+
+    // change the currentTime of the audio so that the play tracker
+    // moves to the new position of the slider, this is the same as the
+    // trimZoneWidth
+    audioStartingPercentage = trimZoneWidth;
 
     // update the start/end times of the audio
     trimStartTime = ((100 - fixedPosLeft_pcnt) / 100) * totalDuration;
@@ -144,10 +205,13 @@ function moveRightSlider(event) {
         Rslider.css('left', fixedPosRight_pcnt + '%');
     }
 
-    // finally, change the width of the Ltrim-zone so it
+    // change the width of the Ltrim-zone so it
     // fills from the left up to the Lslider
     var trimZoneWidth = 100 - fixedPosRight_pcnt;
     RtrimZone.css('width', trimZoneWidth + '%');
+
+    // change the max percentage the track should reach when playing
+    audioMaxPercentage = fixedPosRight_pcnt;
 
     // update the start/end times of the audio
     trimEndTime = (fixedPosRight_pcnt / 100) * totalDuration;
@@ -185,4 +249,11 @@ function updateByteRange() {
     var currentURL = $('a.download').attr('href');
     currentURL = currentURL.split('?')[0];
     $('a.download').attr('href', currentURL + '?startBytes=' + startBytes + '&endBytes=' + endBytes);
+}
+
+function changeCurrentTime(percentage) {
+    if (audio.playing) {
+        audio.pause();
+    }
+    audio.currentTime = audio.duration * (percentage / 100);
 }
